e0bd855c67e203696269c0d8425a575ac6da3559
Bikalpa <bikalpapaudel53@gmail.com>
2023-02-25 18:25:48 +0545

    adding more in about section

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,6 +1,7 @@
 import "./About.css"
 import Svg1 from "./Svg1"
 import My from "./My.png"
+import {FaReact, FaGraduationCap, FaLaptop} from "react-icons/fa"
 
 export default function About(){
     return(
@@ -12,7 +13,22 @@ export default function About(){
                     <div className="about-text">About Me</div>
                     <div className="about-container-2">
                         <div className="profile"><img src={My} alt="my pic" className="my-photo" /></div>
-                        <div className="about-info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde harum tempora corrupti</div>
+                        <div className="about-info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde harum tempora corrupti Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ipsa, blanditiis quisquam delectus magni amet. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident libero eius eum.
+                        </div>
+                    </div>
+                    <div className="about-work">
+                        <div className="about-work1 frontend-work">
+                            <div className="work-icon"><FaReact className="work-icon1"/></div>
+                            <div className="work-text">Frontend Developer</div>
+                        </div>
+                        <div className="about-work1 student-work">
+                            <div className="work-icon"><FaGraduationCap className="work-icon1"/></div>
+                            <div className="work-text">student</div>
+                        </div>
+                        <div className="about-work1 tech-work">
+                            <div className="work-icon"><FaLaptop className="work-icon1"/></div>
+                            <div className="work-text">Tech Geek</div>
+                        </div>
                     </div>
                 </div>
             </div>
