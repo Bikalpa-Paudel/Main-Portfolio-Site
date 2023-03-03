95e5ef42fff579da5495b18737635692bdf81d8f
Bikalpa <bikalpapaudel53@gmail.com>
2023-03-03 20:03:06 +0545

    contact form edit

--- src/components/contact/Form.js
+++ src/components/contact/Form.js
@@ -1,12 +1,46 @@
 import './Form.css'
+import { useState } from 'react'
+
 
 export default function Form(){
+    const [formData, setFormData] = useState({
+        fullName: "", email : "", message: "" }
+        )
+
+        function handleOnChange(event){
+            setFormData(prevData => {
+                return{
+                    ...prevData,
+                    [event.target.name] : event.target.value
+                }
+            });
+            
+        }
+
+
     return(
         <div className="form-container">
             <form action="" className="form">
-                <input type="text" name='fullName' className='input full-name-input' placeholder='Full Name'/>
-                <input type="email" name='email' className='input email-input' placeholder='E-mail Address'/>
-                <textarea name='message' className='input message-input' placeholder='Type Your Message Here'/>
+                <input 
+                    type="text" 
+                    name='fullName' 
+                    className='input full-name-input' 
+                    placeholder='Full Name'
+                    onChange={handleOnChange}
+                    />
+                <input 
+                    type="email" 
+                    name='email' 
+                    className='input email-input' 
+                    placeholder='E-mail Address'
+                    onChange={handleOnChange}
+                    />
+                <textarea 
+                    name='message' 
+                    className='input message-input' 
+                    placeholder='Type Your Message Here'
+                    onChange={handleOnChange}
+                />
                 <div className="msg-btn">
                     <input type="submit" className="input send-btn" value="Send Message"/>
                 </div>
