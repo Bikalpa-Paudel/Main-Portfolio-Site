510ff0ad4a44716434a182139c987417e0c5cb0a
Bikalpa <bikalpapaudel53@gmail.com>
2023-03-04 19:43:22 +0545

    adding email.js and creating working contact from

--- src/components/contact/Form.js
+++ src/components/contact/Form.js
@@ -1,10 +1,11 @@
 import './Form.css'
 import { useState } from 'react'
+import emailjs from "emailjs-com"
 
 
 export default function Form(){
     const [formData, setFormData] = useState({
-        fullName: "", email : "", message: "" }
+        name: "", email : "", message: "" }
         )
 
         function handleOnChange(event){
@@ -17,36 +18,45 @@ export default function Form(){
             
         }
 
-        function submit(){
-            console.log(formData)
+        function submit(e){
+            e.preventDefault();
+            console.log(formData);
+            
+            emailjs.sendForm('service_un4zefy', 'template_bjvjtgk', e.target, 'Py1unXBVTEVJHOven')
+                .then((result) => {
+                    console.log(result.text)
+                }, (error) => {
+                    console.log(error.text);
+                });
+                e.target.reset()
         }
 
 
     return(
         <div className="form-container">
-            <form action="" className="form">
+            <form onSubmit={submit}  className="form">
                 <input 
                     type="text" 
-                    name='fullName' 
+                    name='name' 
                     className='input full-name-input' 
                     placeholder='Full Name'
                     onChange={handleOnChange}
-                    />
+                    required/>
                 <input 
                     type="email" 
                     name='email' 
                     className='input email-input' 
                     placeholder='E-mail Address'
                     onChange={handleOnChange}
-                    />
+                    required/>
                 <textarea 
                     name='message' 
                     className='input message-input' 
                     placeholder='Type Your Message Here'
                     onChange={handleOnChange}
-                />
+                    required/>
                 <div className="msg-btn">
-                    <input onClick={submit} type="button" className="input send-btn" value="Send Message"/>
+                    <input type="submit" className="input send-btn" value="Send Message"/>
                 </div>
             </form>
         </div>
