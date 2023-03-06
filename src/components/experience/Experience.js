5e6daa7a015bf1e91b10d15e6485278430064be8
Bikalpa <bikalpapaudel53@gmail.com>
2023-03-06 15:56:04 +0545

    making own component of skill section

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,4 +1,5 @@
 import './Experience.css'
+import Skill from './Skill'
 
 export default function Experience(){
     return(
@@ -8,24 +9,7 @@ export default function Experience(){
                 <div className="one-btn skill-btn">Skills</div>
                 <div className="one-btn student-btn">Educations</div>
             </div>
-            <div className="skill-line">
-                <div className="point">
-                    <div className='skill-right'>HTML / CSS</div>
-                </div>
-                <div className="point">
-                    <div className="skill-left">JavaScript</div>
-                </div>
-                <div className="point">
-                    <div className="skill-right">React / Redux</div>
-                </div>
-                <div className="point">
-                    <div className="skill-left">NodeJs</div>
-                </div>
-                <div className="point">
-                    <div className="skill-right">Git / Github</div>
-                </div>
-
-            </div>
+            <Skill />
         </section>
     )
 }
---
+++ src/components/experience/Skill.js
@@ -0,0 +1,26 @@
+import './Skill.css'
+
+export default function Skill(){
+    return(
+        <>
+            <div className="skill-line">
+                <div className="point">
+                    <div className='skill-right'>HTML / CSS</div>
+                </div>
+                <div className="point">
+                    <div className="skill-left">JavaScript</div>
+                </div>
+                <div className="point">
+                    <div className="skill-right">React / Redux</div>
+                </div>
+                <div className="point">
+                    <div className="skill-left">NodeJs</div>
+                </div>
+                <div className="point">
+                    <div className="skill-right">Git / Github</div>
+                </div>
+
+            </div>
+        </>
+    )
+}
