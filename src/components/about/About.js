9d4fbb458b655cf7b0cef9994da8e43f61d96a55
Bikalpa <bikalpapaudel53@gmail.com>
2023-03-06 22:13:22 +0545

    adding more style in experience section

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,7 +1,4 @@
 import "./About.css"
-import Svg1 from "./Svg1"
-import My from "./My.png"
-import {FaReact, FaGraduationCap, FaLaptop} from "react-icons/fa"
 
 export default function About(){
     return(
