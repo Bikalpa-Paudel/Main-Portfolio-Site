a0937f772782f7292c9121466c3f924d0a147a70
Bikalpa <bikalpapaudel53@gmail.com>
2023-03-06 16:30:44 +0545

    adding state to manually see skill and education components by clicking buttons

---
+++ src/components/experience/Education.js
@@ -0,0 +1,23 @@
+import './Education.css'
+
+export default function Education() {
+    return (
+        <>
+            <div className="skill-line">
+                <div className="point">
+                    <div className="skill-right">High School</div>
+                    <p className="edu-text-r">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, dicta tenetur maxime nam quasi similique rerum optio ut voluptatem provident!</p>
+                </div>
+                <div className="point">
+                    <div className="skill-left">College</div>
+                    <p className="edu-text-l">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, dicta tenetur maxime nam quasi similique rerum optio ut voluptatem provident!</p>
+                </div>
+                <div className="point">
+                    <div className='skill-right'>Self Learning</div>
+                    <p className="edu-text-r">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, dicta tenetur maxime nam quasi similique rerum optio ut voluptatem provident!</p>
+                </div>
+
+            </div>
+        </>
+    )
+}
--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,15 +1,27 @@
+import Education from './Education'
 import './Experience.css'
 import Skill from './Skill'
+import {useState} from 'react'
 
 export default function Experience(){
+    const [show, setShow] = useState(true)
+    function setTrue(){
+        setShow(true)
+        console.log(true)
+    }
+
+    function setFalse(){
+        setShow(false)
+    }
+
     return(
         <section id="experience">
             <div className='exp-text'>The Experience I Have</div>
             <div className="two-btn">
-                <div className="one-btn skill-btn">Skills</div>
-                <div className="one-btn student-btn">Educations</div>
+                <div onClick={setTrue} className="one-btn skill-btn">Skills</div>
+                <div onClick={setFalse} className="one-btn student-btn">Educations</div>
             </div>
-            <Skill />
+            {show  ? <Skill /> : <Education /> }
         </section>
     )
 }
