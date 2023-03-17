5bbff7506b80883ece8a1d7d441d7f0bcd2a2f65
Bikalpa <bikalpapaudel53@gmail.com>
2023-03-17 12:08:35 +0545

    adding svg and more stuf in about section

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,12 +1,19 @@
 import "./About.css"
 import AboutCard from "./AboutCard"
 import {FaReact, FaGraduationCap, FaLaptop} from 'react-icons/fa';
+import Svg1 from "./Svg1";
 
 export default function About(){
     return(
         <section id="about">
             <div className="about-container">
                 <h1 className="about-text">Know More About Me</h1>
+                <div className="svg-container">
+                    <div className="svg-container-2">   
+                        <Svg1 />
+                        <div className="about-para">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus in voluptas dolor debitis voluptate, sunt sequi adipisci vel cumque eaque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic atque accusantium aut voluptatem deserunt expedita facere sapiente laborum at. Sit aut odit temporibus molestiae obcaecati, ipsum autem quaerat perferendis quos.</div>
+                    </div>
+                </div>
                 <div className='about-card-container'>
                     <AboutCard 
                     title='Frontend Developer' icon={<FaReact />}
