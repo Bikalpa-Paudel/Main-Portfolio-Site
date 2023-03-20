075d9988bc0ab45b4e2319910686c69c774bdcd7
Bikalpa <bikalpapaudel53@gmail.com>
2023-03-20 14:09:53 +0545

    more on navbar roesponsive

--- src/components/navbar/Ham.js
+++ src/components/navbar/Ham.js
@@ -1,14 +1,18 @@
 import './Ham.css'
+import {BsGithub} from 'react-icons/bs'
 
-export default function Ham() {
+export default function Ham(props) {
+
+   
     return (
         <div className='ham'>
             <div className="nav-icon-ham">
-                <li><a href="#home" className="icon home">Home</a></li>
-                <li><a href="#about" className="icon about">About</a></li>
-                <li><a href="#experience" className="icon experience">Experience</a></li>
-                <li><a href="#project" className="icon project">Project</a></li>
-                <li><a href="#contact" className="icon contact">Contact</a></li>
+                <li onClick={props.navFunction}><a href="#home" className="icon home">Home</a></li>
+                <li onClick={props.navFunction}><a href="#about" className="icon about">About</a></li>
+                <li onClick={props.navFunction}><a href="#experience" className="icon experience">Experience</a></li>
+                <li onClick={props.navFunction}><a href="#project" className="icon project">Project</a></li>
+                <li onClick={props.navFunction}><a href="#contact" className="icon contact">Contact</a></li>
+                <li onClick={props.navFunction}><a href="https://github.com/Bikalpa-Paudel" target='blank_'><BsGithub className='github-icon'/></a></li>
             </div>
         </div>
     )
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,27 +3,33 @@ import Logo from './navImages/logo.png'
 import {BsGithub} from 'react-icons/bs'
 import {AiOutlineMenu} from 'react-icons/ai'
 import Ham from './Ham'
-
-
-
-
-
+import {useState} from 'react'
 
 
 export default function Navbar(){
+    const [showNav, setShowNav] = useState(false)
+
+    function setNavFalse(){
+        setShowNav(!showNav)
+    }
+
+    function showNavIcon(){
+        setShowNav(!showNav)
+    }
+
     return(
         <nav className="nav">
             <div className="nav-logo"><img src={Logo} alt="Logo" className="logo" /></div>
             <ul className="nav-icons">
-                <li><a href="#home" className="icon home">Home</a></li>
+                <li><a href="#home" className="icon home" >Home</a></li>
                 <li><a href="#about" className="icon about">About</a></li>
                 <li><a href="#experience" className="icon experience">Experience</a></li>
                 <li><a href="#project" className="icon project">Project</a></li>
                 <li><a href="#contact" className="icon contact">Contact</a></li>
             </ul>
             <div className="github"><a href="https://github.com/Bikalpa-Paudel" target='blank_'><BsGithub className='github-icon'/></a></div>
-            <div className="ham-burger"><AiOutlineMenu className='ham-burger-icon'/></div>
-            <Ham />
+            <div onClick={showNavIcon} className="ham-burger"><AiOutlineMenu className='ham-burger-icon'/></div>
+            {showNav && <Ham navFunction={setNavFalse}/>}
         </nav>
     )
 }
