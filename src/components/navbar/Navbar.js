09022656717e95a2850917d6abbfbdce14ffe3fd
Bikalpa <bikalpapaudel53@gmail.com>
2023-03-20 20:22:48 +0545

    adding cross icon in navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,6 +2,7 @@ import './Navbar.css'
 import Logo from './navImages/logo.png'
 import {BsGithub} from 'react-icons/bs'
 import {AiOutlineMenu} from 'react-icons/ai'
+import {RxCross1} from 'react-icons/rx'
 import Ham from './Ham'
 import {useState} from 'react'
 
@@ -28,7 +29,8 @@ export default function Navbar(){
                 <li><a href="#contact" className="icon contact">Contact</a></li>
             </ul>
             <div className="github"><a href="https://github.com/Bikalpa-Paudel" target='blank_'><BsGithub className='github-icon'/></a></div>
-            <div onClick={showNavIcon} className="ham-burger"><AiOutlineMenu className='ham-burger-icon'/></div>
+            <div onClick={showNavIcon} className="ham-burger">{!showNav ? <AiOutlineMenu className='ham-burger-icon'/> : <RxCross1 className='ham-burger-icon' />} </div>
+            
             {showNav && <Ham navFunction={setNavFalse}/>}
         </nav>
     )
