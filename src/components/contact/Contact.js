ce1d9c789c564575f8fefdb8ff4352ead0deb753
Bikalpa <bikalpapaudel53@gmail.com>
2023-03-25 17:35:03 +0545

    added social icons and email

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,5 +1,6 @@
 import "./Contact.css"
 import Form from "./Form"
+import {BsInstagram, BsTwitter} from 'react-icons/bs'
 
 export default function Contact(){
     return(
@@ -10,6 +11,18 @@ export default function Contact(){
 
                     <iframe title='0' className="map" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1315.06004684759!2d85.33150038172123!3d27.718674744364883!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2snp!4v1676904173818!5m2!1sen!2snp" width="600" height="600" style={{border:0}} allowFullScreen={true} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                     <Form />
+                </div>
+                <div className="social">
+                    <p className="my-email">bikalpapaudel53@gmail.com
+                    </p>
+                    <div className="social-icon">
+                            <a href="https://www.instagram.com/bikalpa_paudel_/?next=%2F">
+                                <BsInstagram className="social-i insta"/>
+                            </a>
+                            <a href="https://twitter.com/bikalp18">
+                                <BsTwitter className="social-i twitter"/>
+                            </a>
+                    </div>
                 </div>
             </div>
         </section>
