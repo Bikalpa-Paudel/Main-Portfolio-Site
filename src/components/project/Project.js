2eb22dab29fc56bf4b6aa1b6ae55df3a84aad167
Bikalpa <bikalpapaudel53@gmail.com>
2023-03-29 18:08:25 +0545

    starting with project section

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -5,11 +5,7 @@ export default function Project(){
     return(
     <section id="project">
         <div className="project-container">
-            <div className="project-info">
-                <h1 className="project-text">Project</h1>
-                <p className="project-para">I guess, building projects while learing is the best way to understand what we need to know. Here are some of my project that I have made. To checkout more small projects visit my github profile</p>
-            </div>
-            <Project1 />
+            <div className="project-text">Projects</div>
         </div>
     </section>
     )
--- src/components/project/Project1.js
+++ src/components/project/Project1.js
@@ -1,13 +1,10 @@
 import './Project1.css'
-import { motion } from 'framer-motion'
 export default function Project1() {
     return (
         <>
-            <motion.div
-                initial={{ opacity: 0, scale: 0.5 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0.5 }}
-            />
+            <div className="project-card">
+                <div className="project-image"></div>
+            </div>
         </>
     )
 }
