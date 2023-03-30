bfb3e866dfc5bec98eec2c00da385919ff50024d
Bikalpa <bikalpapaudel53@gmail.com>
2023-03-30 12:51:53 +0545

    making template to showcase projects

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -4,8 +4,12 @@ import Project1 from './Project1'
 export default function Project(){
     return(
     <section id="project">
+        <div className="project-text">Projects</div>
         <div className="project-container">
-            <div className="project-text">Projects</div>
+            <Project1 />
+            <Project1 />
+            <Project1 />
+            <Project1 />
         </div>
     </section>
     )
--- src/components/project/Project1.js
+++ src/components/project/Project1.js
@@ -4,6 +4,10 @@ export default function Project1() {
         <>
             <div className="project-card">
                 <div className="project-image"></div>
+                <div className="project-btns">
+                    <div className="live-btn project-btn">Live Demo</div>
+                    <div className="code-btn project-btn">Source Code</div>
+                </div>
             </div>
         </>
     )
